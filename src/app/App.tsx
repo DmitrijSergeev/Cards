af81fc2ba1ba5c46a532794736fda8f38e0aae13
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2023-11-27 17:13:58 +0400

    add routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,9 @@
-import { useState } from 'react'
-
-import { Checkbox } from '@/components/ui/checkbox'
+import { Router } from '@/router'
 export function App() {
-  const [checked, setCheked] = useState(false)
-
-  const onchangeHandler = (checked: boolean) => {
-    setCheked(checked)
-  }
-
   return (
     <>
       <div>Hello world!</div>
-      <Checkbox checked={checked} label={'Это наш лейбл'} onChange={onchangeHandler} />
+      return <Router />
     </>
   )
 }
