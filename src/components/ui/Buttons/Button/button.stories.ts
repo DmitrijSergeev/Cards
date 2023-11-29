d66c2cf3a9875939d98503867621f10a1bd97627
Roman <r.rybkin94@gmail.com>
2023-11-30 00:27:40 +0500

    add Button.tsx

--- src/components/ui/Buttons/Button/button.stories.ts
+++ src/components/ui/Buttons/Button/button.stories.ts
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react'
 
 import { iconsExport } from '@/common/icons/iconsExport'
 
-import { Button } from './'
+import { Button } from './index'
 
 const meta = {
   argTypes: {
