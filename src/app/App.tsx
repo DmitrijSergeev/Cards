9405106dfbcd19ab4612b838765e25aa4a44b703
Roman <r.rybkin94@gmail.com>
2023-12-05 12:22:32 +0500

    fix IconButton.module.scss

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,7 @@
-import { IconButton } from '@/components/ui/Buttons/IconButton'
-import { IcEye } from '@/components/ui/IconsComponents/IcEye/IcEye'
 import { Router } from '@/router'
 export function App() {
   return (
     <>
-      <IconButton>
-        <IcEye size={1.4} />
-      </IconButton>
       <div>Hello world!</div>
       return <Router />
     </>
