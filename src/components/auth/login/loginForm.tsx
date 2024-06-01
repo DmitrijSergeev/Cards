8b20a35c98579c9898b32ba837f340f12c31c8ff
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2024-06-01 13:54:31 +0400

    form

---
+++ src/components/auth/login/loginForm.tsx
@@ -0,0 +1,8 @@
+import { ComponentPropsWithoutRef } from 'react'
+
+import s from './loginForm.module.scss'
+
+export type LoginProps = {} & ComponentPropsWithoutRef<'input'>
+const LoginForm = () => {
+  return <div></div>
+}
