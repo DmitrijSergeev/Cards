246125924200f03da948fed804910dbade787a4b
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2024-06-12 10:36:32 +0400

    some adding

--- src/components/ui/select/select.tsx
+++ src/components/ui/select/select.tsx
@@ -1,3 +1,26 @@
-export const Select = () => {
-  return <div></div>
-}
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
+
+import * as SelectRadix from '@radix-ui/react-select'
+import clsx from 'clsx'
+
+import s from './select.module.scss'
+
+export type SelectProps = {
+  className?: string
+  fullWidth?: boolean
+  label?: string
+} & ComponentPropsWithoutRef<typeof SelectRadix.Root>
+
+export const Select = forwardRef<ElementRef<typeof SelectRadix.Root>, SelectProps>(
+  ({ className, fullWidth, label }, ref) => {
+    const classNames = {
+      root: clsx(s.root),
+    }
+
+    return (
+      <SelectRadix.Root>
+        <SelectRadix.Trigger className={classNames.root}></SelectRadix.Trigger>
+      </SelectRadix.Root>
+    )
+  }
+)
