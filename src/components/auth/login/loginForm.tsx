0eea9a93c0b2a69d9e492d10534c2c209d1e3e96
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2024-06-18 21:00:55 +0400

    login

--- src/components/auth/login/loginForm.tsx
+++ src/components/auth/login/loginForm.tsx
@@ -1,8 +1,10 @@
-import { ComponentPropsWithoutRef } from 'react'
+import { ComponentPropsWithoutRef, forwardRef } from 'react'
 
 import s from './loginForm.module.scss'
 
 export type LoginProps = {} & ComponentPropsWithoutRef<'input'>
-export const LoginForm = () => {
+export const LoginForm = forwardRef(() => {
+  const classNames = {}
+
   return <div></div>
-}
+})
